f51823cbd8760b86db65ed9883f2ef5a820b4e97
duck-oli <a01027805597@gmail.com>
2024-07-06 03:58:00 +0900

    new file:   06/.gitignore 	modified:   06/package-lock.json modified:   06/package.json 	modified:   06/public/index.html 	modified:   06/src/App.js 	modified:   06/src/routes/Detail.js

--- 06/src/routes/Detail.js
+++ 06/src/routes/Detail.js
@@ -8,15 +8,15 @@ function Detail() {
   const [movie, setMovie] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const getMovie = async () => {
-    const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
-    setMovie(json.data.movie);
-    setLoading(false);
-  }
-
   useEffect(() => {
+    const getMovie = async () => {
+      const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+      setMovie(json.data.movie);
+      setLoading(false);
+    }
+
     getMovie();
-  }, [])
+  }, [id, movie])
 
   return (
     <div>
